015fd24a03ce71b6e0c45da454d3980a36568320
Jealousmango <jealousmango@gmail.com>
2019-01-22 11:47:29 -0800

    Tutorial complete
    Route to saved suggestions and color changes.

--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,72 @@
+// Add a new route to hold the favorites.
+
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Startup Name Generator',
-        home: RandomWords(),
+    return new MaterialApp(
+      title: 'Startup Name Generator',
+      theme: new ThemeData(
+        primaryColor: Colors.white,
+      ),
+      home: new RandomWords(),
     );
   }
 }
 
-class RandomWordsState extends State<RandomWords> {
+class RandomWords extends StatefulWidget {
   @override
-  final _suggestions = <WordPair>[];
+  RandomWordsState createState() => new RandomWordsState();
+}
+
+class RandomWordsState extends State<RandomWords> {
+  final List<WordPair> _suggestions = <WordPair>[];
   final Set<WordPair> _saved = new Set<WordPair>();
-  final _biggerFont = const TextStyle(fontSize: 18.0);
+  final TextStyle _biggerFont = const TextStyle(fontSize: 18.0);
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Startup Name Generator'),
+    return new Scaffold(
+      appBar: new AppBar(
+        title: const Text('Startup Name Generator'),
+        actions: <Widget>[
+          new IconButton(icon: const Icon(Icons.list), onPressed: _pushSaved),
+        ],
       ),
       body: _buildSuggestions(),
     );
   }
-  Widget _buildSuggestions() {
-    return ListView.builder(
-        padding: const EdgeInsets.all(14.0),
-        itemBuilder: /*1*/ (context, i) {
-          if (i.isOdd) return Divider(); /*2*/
 
-          final index = i ~/ 2; /*3*/
+  Widget _buildSuggestions() {
+    return new ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: (BuildContext _context, int i) {
+          if (i.isOdd) {
+            return const Divider();
+          }
+          final int index = i ~/ 2;
           if (index >= _suggestions.length) {
-            _suggestions.addAll(generateWordPairs().take(10)); /*4*/
+            _suggestions.addAll(generateWordPairs().take(10));
           }
           return _buildRow(_suggestions[index]);
         });
   }
+
   Widget _buildRow(WordPair pair) {
     final bool alreadySaved = _saved.contains(pair);
-    return ListTile(
-      title: Text(
+
+    return new ListTile(
+      title: new Text(
         pair.asPascalCase,
         style: _biggerFont,
       ),
       trailing: new Icon(
-        alreadySaved ? Icons.favorite: Icons.favorite_border,
-        color: alreadySaved ? Colors.red: null,
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
       ),
       onTap: () {
         setState(() {
@@ -57,14 +75,40 @@ class RandomWordsState extends State<RandomWords> {
           } else {
             _saved.add(pair);
           }
-          print("The _saved set is: $_saved");
         });
       },
     );
   }
-}
 
-class RandomWords extends StatefulWidget {
-  @override
-  RandomWordsState createState() => new RandomWordsState();
+  void _pushSaved() {
+    Navigator.of(context).push(
+      new MaterialPageRoute<void>(
+        builder: (BuildContext context) {
+          final Iterable<ListTile> tiles = _saved.map(
+                (WordPair pair) {
+              return new ListTile(
+                title: new Text(
+                  pair.asPascalCase,
+                  style: _biggerFont,
+                ),
+              );
+            },
+          );
+          final List<Widget> divided = ListTile
+              .divideTiles(
+            context: context,
+            tiles: tiles,
+          )
+              .toList();
+          return new Scaffold(
+            appBar: new AppBar(
+              title: const Text('Saved Suggestions'),
+            ),
+            body: new ListView(children: divided),
+          );
+
+        },
+      ),
+    );
+  }
 }
